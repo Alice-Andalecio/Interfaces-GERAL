e5fa6d474f1dc4f7378aa971877828dce83c5787
Luana Pinheiro <pinheiro.luana38@outlook.com>
2017-10-01 16:09:40 -0300

    add js para porcentagem

---
+++ js/cadastroAnimalPorcentagem.js
@@ -0,0 +1,20 @@
+function escondePorcentagem(porcentagem) { //Ao clicar no botão "Fórum", respectivamente aparecerá o fórum da questão.
+        var display = document.getElementById(porcentagem).style.display;
+
+        if(display == "none")
+            document.getElementById(porcentagem).style.display = 'block';
+        else
+            document.getElementById(porcentagem).style.display = 'none';
+    }
+
+function escondePorcentagem2(porcentagem2) { //Ao clicar no botão "Fórum", respectivamente aparecerá o fórum da questão.
+        var display = document.getElementById(porcentagem2).style.display;
+
+        if(display == "none")
+            document.getElementById(porcentagem2).style.display = 'block';
+        else
+            document.getElementById(porcentagem2).style.display = 'none';
+    }
+
+
+
